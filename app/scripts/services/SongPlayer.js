6e79fc57200d94f43cd874bf8c2fe5c7c64ee2fe
clarefahey <fahey.clare@gmail.com>
2017-11-22 13:29:47 -0500

    changes for cp7assignment 1122

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -51,11 +51,10 @@
    SongPlayer.play = function(song) {
          if (currentSong !== song) {
             setSong(song);
-            currentBuzzObject.play();
-            song.playing = true;
+            playSong(song);
         } else if (currentSong === song) {
             if (currentBuzzObject.isPaused()) {
-             currentBuzzObject.play();
+            playSong(song);
           }
       }
   };
